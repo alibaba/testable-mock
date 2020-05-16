44eda2be06ea5ff15c5da022055f4181bb1290bc
金戟 <jinji.lf@alibaba-inc.com>
2020-05-16 12:03:29 +0800

    add pool field to static new class

--- src/main/java/com/alibaba/testable/generator/StaticNewClassGenerator.java
+++ src/main/java/com/alibaba/testable/generator/StaticNewClassGenerator.java
@@ -5,7 +5,9 @@ import com.squareup.javapoet.*;
 
 import javax.lang.model.element.Modifier;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Generate global n.e class code
@@ -18,9 +20,17 @@ public class StaticNewClassGenerator {
         return JavaFile.builder(ConstPool.SN_PKG,
             TypeSpec.classBuilder(ConstPool.SN_CLS)
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addField(buildStaticPoolField())
                 .addMethod(buildStaticNewMethod())
                 .build())
             .build().toString();
+    }
+
+    private FieldSpec buildStaticPoolField() {
+        return FieldSpec.builder(ParameterizedTypeName.get(Map.class, Class.class, Object.class), "pool", Modifier.PUBLIC)
+            .addModifiers(Modifier.STATIC)
+            .initializer("new $T<>()", HashMap.class)
+            .build();
     }
 
     private MethodSpec buildStaticNewMethod() {
@@ -40,6 +50,15 @@ public class StaticNewClassGenerator {
         builder.addStatement("$T<$T> pts = new $T<>()", List.class, Class.class, ArrayList.class)
             .beginControlFlow("for (Object o : args)")
             .addStatement("pts.add(o.getClass())")
+            .endControlFlow()
+            .beginControlFlow("if (!pool.isEmpty())")
+            .beginControlFlow("try")
+            .addStatement("T obj = (T)pool.get(type)")
+            .beginControlFlow("if (obj != null)")
+            .addStatement("return obj")
+            .endControlFlow()
+            .nextControlFlow("catch (Exception e)")
+            .endControlFlow()
             .endControlFlow()
             .beginControlFlow("try")
             .addStatement("return type.getConstructor(pts.toArray(new Class[0])).newInstance(args)")
