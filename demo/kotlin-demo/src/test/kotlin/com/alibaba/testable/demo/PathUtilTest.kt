419a10669d837710d3c09f6ee47b4c1ecc525875
金戟 <jinji.lf@alibaba-inc.com>
2020-10-26 20:39:12 +0800

    add case to test kotlin invoke java method

---
+++ demo/kotlin-demo/src/test/kotlin/com/alibaba/testable/demo/PathUtilTest.kt
@@ -0,0 +1,48 @@
+package com.alibaba.testable.demo
+
+import org.junit.jupiter.api.Test
+import com.alibaba.testable.core.annotation.TestableMock
+import com.alibaba.testable.core.tool.TestableTool.verify
+import java.io.File
+
+class PathUtilTest {
+
+    @TestableMock
+    fun exists(f: File): Boolean {
+        return when (f.absolutePath) {
+            "/a/b" -> true
+            "/a/b/c" -> true
+            else -> f.exists()
+        }
+    }
+
+    @TestableMock
+    fun isDirectory(f: File): Boolean {
+        return when (f.absolutePath) {
+            "/a/b/c" -> true
+            else -> f.isDirectory
+        }
+    }
+
+    @TestableMock
+    fun delete(f: File): Boolean {
+        return true
+    }
+
+    @TestableMock
+    fun listFiles(f: File): Array<File>? {
+        return when (f.absolutePath) {
+            "/a/b" -> arrayOf(File("/a/b/c"), File("/a/b/d"))
+            "/a/b/c" -> arrayOf(File("/a/b/c/e"))
+            else -> f.listFiles()
+        }
+    }
+
+    @Test
+    fun should_able_to_mock_java_method_invoke_in_kotlin() {
+        PathUtil.deleteRecursively(File("/a/b/"))
+        verify("listFiles").times(2)
+        verify("delete").times(4)
+    }
+
+}
