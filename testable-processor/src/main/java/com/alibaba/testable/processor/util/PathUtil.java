580fed7dac1d7e6bc2ab8bf833ebb73c0f0280c9
xuyuqing <xuyuqing.com@bytedance.com>
2021-01-07 10:31:35 +0800

    解决：Windows下文件分隔符替换多重转义的问题，增加本地maven仓库

--- testable-processor/src/main/java/com/alibaba/testable/processor/util/PathUtil.java
+++ testable-processor/src/main/java/com/alibaba/testable/processor/util/PathUtil.java
@@ -5,18 +5,19 @@ public class PathUtil {
     private static final String PREFIX_WIN = "win";
     private static final String PROPERTY_OS_NAME = "os.name";
     private static final String PATH_SPLIT_UNIX = "/";
-    private static final String PATH_SPLIT_WIN = "\\";
+    private static final String PATH_SPLIT_WIN = "\\\\";
     private static final String PROTOCOL_FILE = "file:/";
 
     /**
      * Fit path according to operation system type
+     *
      * @param path original path
      * @return fitted path
      */
     public static String fitPathString(String path) {
         String os = System.getProperty(PROPERTY_OS_NAME);
         if (os.toLowerCase().startsWith(PREFIX_WIN)) {
-            return path.replaceAll(PATH_SPLIT_UNIX, PATH_SPLIT_WIN);
+            path = path.replaceAll(PATH_SPLIT_UNIX, PATH_SPLIT_WIN);
         }
         return path.startsWith(PROTOCOL_FILE) ? path : (PROTOCOL_FILE + path);
     }
