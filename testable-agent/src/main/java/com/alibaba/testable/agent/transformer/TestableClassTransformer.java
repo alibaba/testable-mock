7181b492113de78082106083a941442d1a42df49
金戟 <jinji.lf@alibaba-inc.com>
2021-05-14 21:24:34 +0800

    package prefix should end with a slash

--- testable-agent/src/main/java/com/alibaba/testable/agent/transformer/TestableClassTransformer.java
+++ testable-agent/src/main/java/com/alibaba/testable/agent/transformer/TestableClassTransformer.java
@@ -38,8 +38,8 @@ public class TestableClassTransformer implements ClassFileTransformer {
     /**
      * Just avoid spend time to scan those surely non-user classes, should keep these lists as tiny as possible
      */
-    private final String[] BLACKLIST_PREFIXES = new String[] { "sun/", "com/sun/", "javax/crypto", "java/util/logging",
-        "org/gradle/", "org/robolectric/" };
+    private final String[] BLACKLIST_PREFIXES = new String[] { "sun/", "com/sun/", "javax/crypto/",
+        "java/util/logging/", "org/gradle/", "org/robolectric/" };
 
     public MockClassParser mockClassParser = new MockClassParser();
 
