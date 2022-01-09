02f15177fc380e350582df0af29e7b642642c40d
金戟 <jinji.lf@alibaba-inc.com>
2022-01-09 22:45:14 +0800

    release v0.7.3

--- demo/kotlin-demo/build.gradle.kts
+++ demo/kotlin-demo/build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
-    testImplementation("com.alibaba.testable:testable-all:0.7.2")
-    testAnnotationProcessor("com.alibaba.testable:testable-processor:0.7.2")
+    testImplementation("com.alibaba.testable:testable-all:0.7.3")
+    testAnnotationProcessor("com.alibaba.testable:testable-processor:0.7.3")
 }
 
 tasks.withType<KotlinCompile> {
